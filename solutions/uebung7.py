3a9c93c9fd8705179cddcf1a14c61cf517e85287
mungrogr84298 <munrogr84298@th-nuernberg.de>
2024-05-02 11:15:09 +0200

    uebung filenames fixed

--- solutions/uebung7.py
+++ solutions/uebung7.py
@@ -1,40 +1,19 @@
-# Windows Size
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
 import time
 
 driver = webdriver.Chrome()
-driver.get('http://localhost:8000/web/uebung/7.html')
+driver.get('http://localhost:8000/web/uebung/8.html')
+
 driver.implicitly_wait(1)
-time.sleep(1)
 
-box_element = driver.find_element(By.ID, 'center-box')
+action = ActionChains(driver)
+action.move_to_element(driver.find_element(by=By.ID, value="menu1"))
+action.pause(1)
+action.move_to_element(driver.find_element(by=By.XPATH, value="//a"))
+action.pause(1)
+action.click()
+action.perform()
 
-driver.set_window_size(600, 800)
-# Check Centerbox
-box_width = box_element.value_of_css_property('width')
-box_padding = box_element.value_of_css_property('padding')
-box_width = box_width.replace('px', '')
-box_padding = box_padding.replace('px', '')
-box_width = float(box_width)
-box_padding = float(box_padding)
-assert box_width == 600 - box_padding - 1, f"Width is {box_width}, expected {600 - box_padding - 1}"
-border_radius = box_element.value_of_css_property('border-radius')
-assert border_radius == '0px', f"Border radius is {border_radius}, expected 0px"
-print ("Style sheet is correct for 600p")
-
-driver.set_window_size(1920, 1080)
-driver.implicitly_wait(1)
-#time.sleep(100)
-box_width = box_element.value_of_css_property('width')
-box_padding = box_element.value_of_css_property('padding')
-print (box_width)
-print (box_padding)
-box_width = box_width.replace('px', '')
-box_padding = box_padding.replace('px', '')
-box_width = float(box_width)
-box_padding = float(box_padding)
-assert box_width == 1920 * 0.75 - box_padding - 1, f"Width is {box_width}, expected {1920 * 0.75 - box_padding - 1}"
-border_radius = box_element.value_of_css_property('border-radius')
-assert border_radius == '8px', f"Border radius is {border_radius}, expected 0px"
-print ("Stylesheet is correct for 1920p")
+time.sleep(2)
