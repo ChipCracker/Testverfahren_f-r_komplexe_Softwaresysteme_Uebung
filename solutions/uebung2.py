6a5261081185b4d8baaf8155a8f519ab290cdfee
Christopher Witzl <43088334+ChipCracker@users.noreply.github.com>
2024-05-03 09:46:35 +0200

    update

--- solutions/uebung2.py
+++ solutions/uebung2.py
@@ -12,27 +12,27 @@ def search_selenium_official_website():
 
         driver.implicitly_wait(10) 
         try:
-            # deny cookies
+            # Cookies ablehnen
             accept_button = driver.find_element(By.XPATH, '//*[@id="W0wltc"]')
             accept_button.click()
         except Exception as e:
             print("No privacy button found, continuing...")
 
-        # Locate the search box, input "Selenium WebDriver", "q" is always the element name
+        # Google Suchfeld ansteuern und Suchbegriff eingeben
         search_box = driver.find_element(By.NAME, "q")
         search_box.send_keys("Selenium WebDriver")
         search_box.send_keys(Keys.RETURN)
 
-        # Wait for the search results 
+        # auf die Ergebnisseite warten
         driver.implicitly_wait(10)  
 
-        # Find and click the official Selenium website link
+        # Klicke auf den Link zur offiziellen Selenium Webseite
         link = driver.find_element(By.XPATH,  "//a[h3[contains(text(),'WebDriver')]]")
         link.click()
 
         print(f"The title of the page is: '{driver.title}'")
 
-         # Check if the URL is as expected
+         # Überprüfe, ob die URL korrekt ist
         expected_url = "https://www.selenium.dev/documentation/webdriver/"
         current_url = driver.current_url
         assert current_url == expected_url, f"URL mismatch: expected {expected_url}, got {current_url}"
